9a252bf9c0e393c4d19938443c8567f719cb195f
Chris Lormor <clormor@palantir.com>
2020-10-02 22:14:06 +0100

    shorten top text

--- src/EtchASketch.js
+++ src/EtchASketch.js
@@ -1,24 +1,25 @@
 import React from "react";
-import EtchASketchGrid from "./EtchASketchGrid"
+import EtchASketchGrid from "./EtchASketchGrid";
 
 export default function EtchASketch() {
-  const message =
-    `I hope you enjoy playing Etch-A-Sketch! ` +
-    `This was developed whilst learning HTML/CSS through `;
+  const message = `This etch-a-sketch game was written for `;
   return (
     <main role="main" className="flex-shrink-0">
       <section className="jumbotron text-center">
         <div className="container">
           <p className="lead text-muted">
             {message}
-            <a href="https://www.theodinproject.com" className="link text-decoration-none">
+            <a
+              href="https://www.theodinproject.com"
+              className="link text-decoration-none"
+            >
               The Odin Project
             </a>
           </p>
         </div>
       </section>
 
-      <div className="album py-5">
+      <div className="album py-3">
         <EtchASketchGrid />
       </div>
     </main>
