8166cec5e4b449984636c3ed4a18c7cef4443a9c
Chris Lormor <clormor@palantir.com>
2020-10-02 23:49:45 +0100

    separate page logo from title

--- src/Header.js
+++ src/Header.js
@@ -4,8 +4,8 @@ export default function Header(props) {
   return (
     <nav className="navbar navbar-dark bg-dark">
       <div className="container d-flex justify-content-between">
-        <div class="navbar-brand d-flex align-items-center">
-          {props.image}
+        <div className="navbar-brand d-flex align-items-center">
+          <div className="pr-3">{props.image}</div>
           <strong>{props.title}</strong>
         </div>
       </div>
